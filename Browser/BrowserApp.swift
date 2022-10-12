0b1c2dff887758cc761206b93be1d2e5ffaab08f
Zhen Luo <zhenluofrank@gmail.com>
2022-10-11 21:46:52 -0400

    Initial Commit

---
+++ Browser/BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowserApp.swift
+//  Browser
+//
+//  Created by Zhen Luo on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
